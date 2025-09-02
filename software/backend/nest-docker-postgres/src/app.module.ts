04c02a523809b5fb3f2197791bc76b4ce7dd0025
CarlosPel <carlossanllon@gmail.com>
2025-09-02 15:16:05 +0200

    feat: add TypeORM and configuration support for PostgreSQL

--- software/backend/nest-docker-postgres/src/app.module.ts
+++ software/backend/nest-docker-postgres/src/app.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        host: config.get('DATABASE_HOST'),
+        port: +config.get<number>('DATABASE_PORT', 5432),
+        username: config.get('DATABASE_USER'),
+        password: config.get('DATABASE_PASSWORD'),
+        database: config.get('DATABASE_NAME'),
+        autoLoadEntities: true,
+        synchronize: true, // ⚠️ Borrar para despliegue en producción
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
